034df4064ff53a8e605898f4c9e08279f6a43941
J-eris <erickxt@hotmail.com>
2023-03-27 17:35:24 -0600

    Actualización de estyle

--- script.js
+++ script.js
@@ -1,120 +1,119 @@
 
-        const form = document.getElementById("formFormulario");
-        form.addEventListener("submit", function(event) {
-            event.preventDefault(); 
-            let formFormularioData = new FormData(form);
-            let formFormularioObj = convertFormFormularioData(formFormularioData);
-            saveFormFormularioObj(formFormularioObj)
-            insertTableRow(formFormularioObj);
-            form.reset();
-            //let formTableRef = document.getElementById("formTable");
-            // let newTableRow = formTableRef.insertRow(-1);
+const form = document.getElementById("formFormulario");
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    let formFormularioData = new FormData(form);
+    let formFormularioObj = convertFormFormularioData(formFormularioData);
+    saveFormFormularioObj(formFormularioObj)
+    insertTableRow(formFormularioObj);
+    // form.reset();
+    //let formTableRef = document.getElementById("formTable");
+    // let newTableRow = formTableRef.insertRow(-1);
 
-            // let newTableCell = newTableRow.insertCell(0);
-            // newTableCell.textContent = formFormularioData.get("nombre");
-            
-            // newTableCell = newTableRow.insertCell(1);
-            // newTableCell.textContent = formFormularioData.get("raza");  
+    // let newTableCell = newTableRow.insertCell(0);
+    // newTableCell.textContent = formFormularioData.get("nombre");
 
-            // newTableCell = newTableRow.insertCell(2);
-            // newTableCell.textContent = formFormularioData.get("color"); 
+    // newTableCell = newTableRow.insertCell(1);
+    // newTableCell.textContent = formFormularioData.get("raza");  
 
-            // newTableCell = newTableRow.insertCell(3);
-            // newTableCell.textContent = formFormularioData.get("edad"); 
-        })
+    // newTableCell = newTableRow.insertCell(2);
+    // newTableCell.textContent = formFormularioData.get("color"); 
 
-        document.addEventListener("DOMContentLoaded", function(event) {
-            drawCategory()
-            let transactionObjArray = JSON.parse(localStorage.getItem("fromData"))
-            transactionObjArray.forEach(
-                function(arrayElement) {
-                    insertTableRow(arrayElement)
-                }
-            )              
-                     
-        });
+    // newTableCell = newTableRow.insertCell(3);
+    // newTableCell.textContent = formFormularioData.get("edad"); 
+})
 
-        function drawCategory() {
-            let allCategories = ["bulldog", "dog", "bulling", "chiguagua"]
-            for (let index = 0; index < allCategories.length; index++) {
-                insertCategory(allCategories[index]);
-                
-            }
+document.addEventListener("DOMContentLoaded", function (event) {
+    drawCategory()
+    let transactionObjArray = JSON.parse(localStorage.getItem("fromData"))
+    transactionObjArray.forEach(
+        function (arrayElement) {
+            insertTableRow(arrayElement)
         }
+    )
 
-        function insertCategory(categoryName) {
-            const selectElement = document.getElementById("raza")
-            let htmlToInsert = `<option> ${categoryName} </option>`
-            selectElement.insertAdjacentHTML("beforeend", htmlToInsert) 
-        }
+});
 
-        function getNewTransactionId() {
-                let lastTransactionId = localStorage.getItem("lastTransactionId") || "-1";
-                let newTransactionId = JSON.parse(lastTransactionId) + 1;
-                localStorage.setItem("lastTransactionId", JSON.stringify(newTransactionId))
-                return newTransactionId;
-            }
+function drawCategory() {
+    let allCategories = ["bulldog", "dog", "bulling", "chiguagua"]
+    for (let index = 0; index < allCategories.length; index++) {
+        insertCategory(allCategories[index]);
 
-        function convertFormFormularioData(formFormularioData) {
-            let nombre = formFormularioData.get("nombre");
-            let raza = formFormularioData.get("raza");
-            let color = formFormularioData.get("color");
-            let edad = formFormularioData.get("edad");
-            let transactionId = getNewTransactionId();
-            return{"nombre": nombre, "raza": raza, "color": color, "edad": edad, "transactionId": transactionId}
-        }
+    }
+}
 
-        // Es lo mismo que lo anterior pero más limpio en una función
-        function insertTableRow(formFormularioObj) {
-            let formTableRef = document.getElementById("formTable");
-            let newTableRow = formTableRef.insertRow(-1);
-            newTableRow.setAttribute("data-transaction-Id", formFormularioObj["transactionId"])
+function insertCategory(categoryName) {
+    const selectElement = document.getElementById("raza")
+    let htmlToInsert = `<option> ${categoryName} </option>`
+    selectElement.insertAdjacentHTML("beforeend", htmlToInsert)
+}
 
-            let newTableCell = newTableRow.insertCell(0);
-            newTableCell.textContent = formFormularioObj["nombre"];
+function getNewTransactionId() {
+    let lastTransactionId = localStorage.getItem("lastTransactionId") || "-1";
+    let newTransactionId = JSON.parse(lastTransactionId) + 1;
+    localStorage.setItem("lastTransactionId", JSON.stringify(newTransactionId))
+    return newTransactionId;
+}
 
-            newTableCell = newTableRow.insertCell(1);   
-            newTableCell.textContent = formFormularioObj["raza"];  
+function convertFormFormularioData(formFormularioData) {
+    let nombre = formFormularioData.get("nombre");
+    let raza = formFormularioData.get("raza");
+    let color = formFormularioData.get("color");
+    let edad = formFormularioData.get("edad");
+    let transactionId = getNewTransactionId();
+    return { "nombre": nombre, "raza": raza, "color": color, "edad": edad, "transactionId": transactionId }
+}
 
-            newTableCell = newTableRow.insertCell(2);
-            newTableCell.textContent = formFormularioObj["color"]; 
+// Es lo mismo que lo anterior pero más limpio en una función
+function insertTableRow(formFormularioObj) {
+    let formTableRef = document.getElementById("formTable");
+    let newTableRow = formTableRef.insertRow(-1);
+    newTableRow.setAttribute("data-transaction-Id", formFormularioObj["transactionId"])
 
-            newTableCell = newTableRow.insertCell(3);
-            newTableCell.textContent = formFormularioObj["edad"]; 
+    let newTableCell = newTableRow.insertCell(0);
+    newTableCell.textContent = formFormularioObj["nombre"];
 
-            let newDeleteCell = newTableRow.insertCell(4);
-            let deleteButton = document.createElement("button");
-            deleteButton.textContent = "Eliminar";
-            newDeleteCell.appendChild(deleteButton);
+    newTableCell = newTableRow.insertCell(1);
+    newTableCell.textContent = formFormularioObj["raza"];
 
-            deleteButton.addEventListener("click", (event) => {
-                let transactionRow = event.target.parentNode.parentNode;
-                let transactionId = transactionRow.getAttribute("data-transaction-Id")         
-                transactionRow.remove();
-                deleteTransactionObj(transactionId);
-            })
-        }
+    newTableCell = newTableRow.insertCell(2);
+    newTableCell.textContent = formFormularioObj["color"];
 
-        //Para pasarle el transactionId de la transaccion que quiero eliminar
-        function deleteTransactionObj(transactionId) {
-            //Obtengo las transacciones de mi base de datos (desconvierto de JSON a objeto)
-            let transactionObjArray = JSON.parse(localStorage.getItem("fromData"))
-            //Busco el indice de la transaccion que quiero eliminar
-            let transactionIndexArray = transactionObjArray.findIndex(element => element.transactionId === transactionId)
-            //Elimino el elemento de la transaccion seleccionada
-            transactionObjArray.splice(transactionIndexArray, 1)
-            //Convierto nuevamente a formato JSON
-            let transactionArrayjJSON = JSON.stringify(transactionObjArray);
-            //Guardo mi array de Transacciones en formato JSON en el localStarage
-            localStorage.setItem("fromData", transactionArrayjJSON);
-        }
+    newTableCell = newTableRow.insertCell(3);
+    newTableCell.textContent = formFormularioObj["edad"];
 
-        function saveFormFormularioObj(formFormularioObj) {
-            let myTransactionArray = JSON.parse(localStorage.getItem("fromData")) || [];
-            myTransactionArray.push(formFormularioObj);
-            //Convierto mi array de transacciones a .JSON
-            let transactionArrayjJSON = JSON.stringify(myTransactionArray);
-            //Guardo mi array de Transacciones en formato JSON en localStarage
-            localStorage.setItem("fromData", transactionArrayjJSON);
-        }
-        
+    let newDeleteCell = newTableRow.insertCell(4);
+    let deleteButton = document.createElement("button");
+    deleteButton.textContent = "Eliminar";
+    newDeleteCell.appendChild(deleteButton);
+
+    deleteButton.addEventListener("click", (event) => {
+        let transactionRow = event.target.parentNode.parentNode;
+        let transactionId = transactionRow.getAttribute("data-transaction-Id")
+        transactionRow.remove();
+        deleteTransactionObj(transactionId);
+    })
+}
+
+//Para pasarle el transactionId de la transaccion que quiero eliminar
+function deleteTransactionObj(transactionId) {
+    //Obtengo las transacciones de mi base de datos (desconvierto de JSON a objeto)
+    let transactionObjArray = JSON.parse(localStorage.getItem("fromData"))
+    //Busco el indice de la transaccion que quiero eliminar
+    let transactionIndexArray = transactionObjArray.findIndex(element => element.transactionId === transactionId)
+    //Elimino el elemento de la transaccion seleccionada
+    transactionObjArray.splice(transactionIndexArray, 1)
+    //Convierto nuevamente a formato JSON
+    let transactionArrayjJSON = JSON.stringify(transactionObjArray);
+    //Guardo mi array de Transacciones en formato JSON en el localStarage
+    localStorage.setItem("fromData", transactionArrayjJSON);
+}
+
+function saveFormFormularioObj(formFormularioObj) {
+    let myTransactionArray = JSON.parse(localStorage.getItem("fromData")) || [];
+    myTransactionArray.push(formFormularioObj);
+    //Convierto mi array de transacciones a .JSON
+    let transactionArrayjJSON = JSON.stringify(myTransactionArray);
+    //Guardo mi array de Transacciones en formato JSON en localStarage
+    localStorage.setItem("fromData", transactionArrayjJSON);
+}
